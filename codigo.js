301357c14524a7979e4a4d4bbaf70b44ed1b45d6
jonharki <jonharki@gmail.com>
2016-09-07 14:15:01 -0700

    tp2

---
+++ codigo.js
@@ -0,0 +1,8 @@
+var dia = (prompt ('ingrese dia de la semana'));
+var diasHabiles = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes'];
+var finDeSemana = ['sabado', 'domingo'];
+for (var i = 0; i < diasHabiles.length; i++) {
+  if ((diasHabiles[i]) === (dia))  alert('Hoy es un dia habil') }; 
+for (var i = 0; i < finDeSemana.length; i++) {
+  if ((finDeSemana[i]) === (dia))  alert('Hoy es fin de semana') }; 
+   
